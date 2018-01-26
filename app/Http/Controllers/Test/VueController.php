730175e86fdf88fe26699154dd769186b23cd997
ychy00001 <yangchunyu00001@163.com>
2018-01-26 11:58:41 +0800

    博客基础环境搭建，后台laravel-admin基础搭建

---
+++ app/Http/Controllers/Test/VueController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Test;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class VueController extends Controller
+{
+    public function testVue(){
+        return view('test.test_vue',[
+            'hello' => 'la_la'
+        ]);
+    }
+}
