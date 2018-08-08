de3455390473ca9f1092a976344d654a9cf7c07a
ychy00001 <yangchunyu00001@163.com>
2018-08-08 14:26:01 +0800

    添加文章详情页面

--- resources/assets/js/components/ArticleComponent.vue
+++ resources/assets/js/components/ArticleComponent.vue
@@ -1,5 +1,173 @@
 <template>
+    <section id="page">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-sm-8 column">
+                    <article class="post clearfix">
+                        <header>
+                            <div class="media">
+                                <a href="./文章详情_files/1(2).jpg" data-lightbox="photo"><img src="./文章详情_files/1(2).jpg" alt=""></a>
+                            </div>
+                            <h3><a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html">My journey into the unknown.</a></h3>
+                            <span>December 22, 2014 / by <a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#">studio-themes</a> / in: <a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#">Travel</a> / <a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#">4 Comments</a></span>
+                        </header>
+                        <div class="editor-styles">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget turpis pulvinar, tempor odio sed, adipiscing dolor. Donec nisi velit, malesuada id dolor non, elementum auctor arcu.</p>
+                            <blockquote>Etiam ultrices viverra diam, eget tristique dui feugiat in. Aenean vitae dictum lacus, non malesuada dui. Curabitur eleifend, mi in dignissim pharetra, turpis neque facilisis nibh.
+                            </blockquote>
+                            <p>Phasellus tempor arcu eros, et bibendum arcu euismod in. Phasellus quis nisi ut dolor pretium aliquet non ullamcorper ipsum. Integer eleifend est nec arcu sagittis, eget ornare metus bibendum. Suspendisse a erat scelerisque tellus condimentum pellentesque eget ut leo. Cras ac luctus enim. In lacinia bibendum auctor. Ut et venenatis metus, at porta metus.</p>
 
+                            <p>Integer commodo, tortor quis pulvinar fringilla, neque magna laoreet neque, a eleifend diam mi at metus. Duis lacinia felis non sapien elementum, id blandit metus sodales.</p>
+                        </div>
+                        <footer>
+                            <div>
+                                <a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#" class="social-1"><i class="fa fa-facebook"></i></a>
+                                <a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#" class="social-1"><i class="fa fa-twitter"></i></a>
+                                <a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#" class="social-1"><i class="fa fa-google-plus"></i></a>
+                                <a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#" class="social-1"><i class="fa fa-pinterest"></i></a>
+                                <a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#" class="social-1"><i class="fa fa-tumblr"></i></a>
+                            </div>
+                            <hr>
+                        </footer>
+                    </article>
+                    <div id="post-author">
+                        <img src="./文章详情_files/avatar2.png" alt="">
+                        <h4>Hi, I’m studio-themes</h4>
+                        <p>Egestas adipiscing purus elementum risus turpis tincidunt, nascetur a, ultricies lacus nisi platea risus sed tincidunt adipiscing.</p>
+                    </div>
+                    <nav id="post-nav-2" class="clearfix">
+                        <div class="push-left">
+                            <a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#">« Previous</a>
+                            <h6>Hike in mountain</h6>
+                        </div>
+                        <div class="push-right">
+                            <a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#">Next »</a>
+                            <h6>Sunset in summer.</h6>
+                        </div>
+                    </nav>
+                    <div id="comments">
+                        <h4>4 Comments</h4>
+                        <ul id="list-comments">
+                            <li class="comment byuser even thread-even depth-1">
+                                <div class="comment-body">
+                                    <div class="comment-author vcard">
+                                        <img src="./文章详情_files/avatar.png" alt="">
+                                        <cite class="fn">John Doe</cite>
+                                    </div>
+                                    <p>Ac amet turpis diam turpis dolor, parturient sit. Porttitor risus. Turpis in aenean nunc cum natoque nec. Mattis urna in, montes et?</p>
+                                    <div class="comment-meta commentmetadata"><a href="http://andon-wordpress.studio-themes.com/338/#comment-43">
+                                        December 22, 2014 at 11:45 pm /</a>&nbsp;&nbsp;<a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#" class="comment-edit-link">Reply</a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="comment byuser even thread-even depth-1">
+                                <div class="comment-body">
+                                    <div class="comment-author vcard">
+                                        <img src="./文章详情_files/avatar.png" alt="">
+                                        <cite class="fn">Chris Cilinis</cite>
+                                    </div>
+                                    <p>Phasellus quis nisi ut dolor pretium aliquet non ullamcorper ipsum. Integer eleifend est nec arcu sagittis, eget ornare metus bibendum.</p>
+                                    <div class="comment-meta commentmetadata"><a href="http://andon-wordpress.studio-themes.com/338/#comment-43">
+                                        December 22, 2014 at 11:45 pm /</a>&nbsp;&nbsp;<a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#" class="comment-edit-link">Reply</a>
+                                    </div>
+                                </div>
+                                <ul class="children">
+                                    <li class="comment byuser even thread-even depth-1">
+                                        <div class="comment-body">
+                                            <div class="comment-author vcard">
+                                                <img src="./文章详情_files/avatar2.png" alt="">
+                                                <cite class="fn">studio-themes (Reply)</cite>
+                                            </div>
+                                            <p>Adipiscing aliquam a odio phasellus auctor penatibus, ac facilisis. Hac est tortor vel mid.</p>
+                                            <div class="comment-meta commentmetadata"><a href="http://andon-wordpress.studio-themes.com/338/#comment-43">
+                                                December 22, 2014 at 13:53 pm /</a>&nbsp;&nbsp;<a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#" class="comment-edit-link">Reply</a>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="comment byuser even thread-even depth-1">
+                                <div class="comment-body">
+                                    <div class="comment-author vcard">
+                                        <img src="./文章详情_files/avatar.png" alt="">
+                                        <cite class="fn">Alan Smith</cite>
+                                    </div>
+                                    <p>Maecenas scelerisque nibh nec eros mattis id suscipit sem viverra estibulum nec est nec urna.</p>
+                                    <div class="comment-meta commentmetadata"><a href="http://andon-wordpress.studio-themes.com/338/#comment-43">
+                                        December 23, 2014 at 8:00 am /</a>&nbsp;&nbsp;<a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#" class="comment-edit-link">Reply</a>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                        <div id="commentform">
+                            <h4>Leave a reply</h4>
+                            <form method="post">
+                                <input type="text" name="personal" placeholder="Name*" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABHklEQVQ4EaVTO26DQBD1ohQWaS2lg9JybZ+AK7hNwx2oIoVf4UPQ0Lj1FdKktevIpel8AKNUkDcWMxpgSaIEaTVv3sx7uztiTdu2s/98DywOw3Dued4Who/M2aIx5lZV1aEsy0+qiwHELyi+Ytl0PQ69SxAxkWIA4RMRTdNsKE59juMcuZd6xIAFeZ6fGCdJ8kY4y7KAuTRNGd7jyEBXsdOPE3a0QGPsniOnnYMO67LgSQN9T41F2QGrQRRFCwyzoIF2qyBuKKbcOgPXdVeY9rMWgNsjf9ccYesJhk3f5dYT1HX9gR0LLQR30TnjkUEcx2uIuS4RnI+aj6sJR0AM8AaumPaM/rRehyWhXqbFAA9kh3/8/NvHxAYGAsZ/il8IalkCLBfNVAAAAABJRU5ErkJggg==&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%;">
+                                <input type="text" name="email" placeholder="Email*">
+                                <input type="text" name="website" placeholder="Website">
+                                <textarea name="comment" placeholder="Message"></textarea>
+                                <button type="submit">Send messsage</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-4 column space">
+                    <aside id="sidebar">
+                        <div class="widget">
+                            <div class="search-form clearfix">
+                                <input type="text" name="s" placeholder="Search...">
+                                <button type="submit"><i class="fa fa-search"></i></button>
+                            </div>
+                        </div>
+                        <div class="widget">
+                            <h4>About</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget turpis pulvinar, tempor odio sed, adipiscing dolor.</p>
+                        </div>
+                        <div class="widget">
+                            <h4>Find me on</h4>
+                            <p>
+                                <a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#" class="social-1"><i class="fa fa-facebook"></i></a>
+                                <a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#" class="social-1"><i class="fa fa-twitter"></i></a>
+                                <a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#" class="social-1"><i class="fa fa-google-plus"></i></a>
+                                <a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#" class="social-1"><i class="fa fa-linkedin"></i></a>
+                            </p>
+                        </div>
+                        <div class="widget">
+                            <h4>Latest Posts</h4>
+                            <ul>
+                                <li><a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#">My journey into the unkown.</a></li>
+                                <li><a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#">Top 10 restaurant in California.</a></li>
+                                <li><a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#">Sunset of summer.</a></li>
+                                <li><a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#">Hike in mountains.</a></li>
+                            </ul>
+                        </div>
+                        <div class="widget">
+                            <h4>Categories</h4>
+                            <ul>
+                                <li><a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/category_travel.html">Travel</a> (4)</li>
+                                <li><a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/category_lifestyle.html">Lifestyle</a> (3)</li>
+                                <li><a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/category_video.html">Video</a> (1)</li>
+                                <li><a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/category_music.html">Music</a> (5)</li>
+                            </ul>
+                        </div>
+                        <div class="widget tagcloud">
+                            <h4>Tags</h4>
+                            <a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#">travel</a>
+                            <a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#">blog</a>
+                            <a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#">lifestyle</a>
+                            <a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#">feature</a>
+                            <a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#">mountain</a>
+                            <a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#">design</a>
+                            <a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#">restaurant</a>
+                            <a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#">journey</a>
+                            <a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#">classic</a>
+                            <a href="http://view.jqueryfuns.com/%E9%A2%84%E8%A7%88-/2015/1/14/706c9c78623f129a044220c0ad3c2013/single.html#">sunset</a>
+                        </div>
+                    </aside>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
